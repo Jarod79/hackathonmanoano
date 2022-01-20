efd064d7e08b9ba9fad05ae258a622271569da37
Jarod79 <89936533+Jarod79@users.noreply.github.com>
2022-01-20 13:24:03 +0100

    update link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import Mano from "../assets/mano.png";
 import Burger from "../assets/burger.png";
+import Cart from "../assets/cart.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header__firstline">
-        <img src={Mano} alt="" />
+        <img src={Mano} alt="mano" className="header__firstline__mano" />
         <input
           type="text"
           placeholder="Rechercher un produit, une marque ou un conseil"
@@ -15,10 +16,16 @@ const Header = () => {
           Bonjour
           <br /> Identifiez-vous
         </p>
+        <img src={Cart} alt="cart" className="header__firstline__cart" />
       </div>
       <div className="header__link">
-        <img src={Burger} alt="burger" />
+        <img src={Burger} alt="burger" className="header__link__burger" />
         <Link to="/">Menu</Link>
+        <img
+          src="https://www.manomano.fr/assets/img/themes/default/svg/link-icons/sales-gear-icon.svg"
+          alt="soldes"
+          className="header__link__soldes"
+        />
         <Link to="/">Soldes</Link>
         <Link to="/">Bon PLans</Link>
         <Link to="/">Meilleures ventes</Link>
