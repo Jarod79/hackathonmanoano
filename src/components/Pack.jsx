98f12de93ae2cb15fea2665383394649c00da65d
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2022-01-20 17:02:52 +0100

    composant pack

--- src/components/Pack.jsx
+++ src/components/Pack.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const Pack = () => {
   return (
-    <div>
-      <img
-        src="https://www.schelfhout.com/templates/yootheme/cache/225940-225939-226589_carrelage-interieur-sol-et-mur-57ffb646.jpeg"
-        alt=""
-      />
-      <h2>Le premier produit</h2>
-      <h5>200€</h5>
+    <div className="pack">
+      <div className="pack__container">
+        <img
+          className="pack__container__img"
+          src="https://www.schelfhout.com/templates/yootheme/cache/225940-225939-226589_carrelage-interieur-sol-et-mur-57ffb646.jpeg"
+          alt=""
+        />
+      </div>
+
+      <div className="pack__desc">
+        <h4 className="pack__desc__title">Le premier produit</h4>
+        <h3 className="pack__desc__price">200€</h3>
+      </div>
     </div>
   );
 };
